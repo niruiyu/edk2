e4b00d44260aa14b178fd08d8a72f0bb4177d2a0
Ashraf Javeed <ashraf.javeed@intel.com>
2020-02-07 13:08:12 +0530

    PciBusDxe: New PCI Express feature Max_Payload_Size
    BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=2194
    
    The code changes are made to enable the configuration of new PCI Express
    feature Max_Payload_Size (MPS), which defines the data packet size for
    the PCI transactions, as per the PCI Base Specification 4 Revision 1.
    
    The code changes are made to calibrate highest common value that is appl-
    icable to all the child nodes originating from the primary root bridge
    device instance.
    
    This programming of MPS is based on each PCI device's capability, and also
    its device-specific platform policy obtained using the new PCI Express
    Platform Protocol interface, defined in the below feature request:-
    https://bugzilla.tianocore.org/show_bug.cgi?id=1954
    
    Signed-off-by: Ashraf Javeed <ashraf.javeed@intel.com>
    Cc: Jian J Wang <jian.j.wang@intel.com>
    Cc: Hao A Wu <hao.a.wu@intel.com>
    Cc: Ray Ni <ray.ni@intel.com>

--- MdeModulePkg/Bus/Pci/PciBusDxe/PciBus.h
+++ MdeModulePkg/Bus/Pci/PciBusDxe/PciBus.h
@@ -288,6 +288,7 @@ struct _PCI_IO_DEVICE {
   // This field is used to support this case.
   //
   UINT16                                    BridgeIoAlignment;
+  UINT8                                     SetupMPS;
 };
 
 #define PCI_IO_DEVICE_FROM_PCI_IO_THIS(a) \
